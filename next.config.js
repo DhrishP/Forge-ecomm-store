71ccf8068c56c226ba516f9c663434c6c8d61473
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2024-02-02 16:38:14 +0530

    changed PWA config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  cacheOnFrontEndNav: true,
   reloadOnOnline:true,
   source:"/app",
   register: true,
   sw: "service-worker.js",
   disable: process.env.NODE_ENV === "development",
-  workboxOptions:{
-    disableDevLogs: true,
-  }
+  cacheStartUrl:true,
 });
 const nextConfig = {
   images: {
