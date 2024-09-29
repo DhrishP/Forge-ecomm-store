2adf65eb74634f1f77fd48f2f99ee2f24e30f2db
DhrishP <parekhdhrish.pg@gmail.com>
2024-09-29 10:46:52 +0530

    fix hydration error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Link from "next/link";
 import Mainnav from "./main-nav";
@@ -10,7 +9,7 @@ const Navbar = async () => {
   const data = await getCategories();
 
   return (
-    <div className="navbar bg-base-100 border-b-gray-500 border">
+    <div className="navbar bg-base-100 border-b-gray-500 border h-16">
       <div className="navbar-start">
         <div className="dropdown ">
           <label tabIndex={0} className="btn text-accent btn-ghost lg:hidden">
@@ -39,15 +38,26 @@ const Navbar = async () => {
             />
           </ul>
         </div>
-        <Link href="/" className="  font-black ml-4   text-2xl">
-          FORGESTORE
+        <Link href="/" className="font-black ml-2 text-2xl flex items-center h-16 overflow-hidden">
+          TinkerLab🧪
+          <span className="text-primary text-sm flex items-center ml-2">
+            <span className="text-sm">by</span>
+            <a
+              href="https://dhrishp1.whyfied.me"
+              className="hover:underline text-primary ml-0.5 whitespace-nowrap"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              whycurious
+            </a>
+          </span>
         </Link>
-        <div className=" hidden lg:flex ml-4 font-semibold  ">
+        <div className=" hidden lg:flex ml-4 font-semibold  h-16 items-center">
           <Mainnav className="items-center space-x-4  px-1" data={data} />
         </div>
       </div>
 
-      <div className="navbar-end mr-1 lg:mr-5 md:mr-3 gap-x-2 flex items-center  ">
+      <div className="navbar-end mr-1 lg:mr-5 md:mr-3 gap-x-2 flex items-center  h-16">
         <ThemeButton/>
         <ShoppingIcon />
       </div>
